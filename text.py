04daf86cdff851f47c17912ac12d07dd23f7dbe2
Benjamin Golder <benjamin.j.golder@gmail.com>
2014-02-05 22:05:15 -0500

    started adding textblock object, realized I need a cached monad for context to measure text boudning boxes

--- text.py
+++ text.py
@@ -1,11 +1,20 @@
-
-
 class TextBlock(object):
+    """ For drawing text
     """
-    """
-    def __init__(self):
+    def __init__(self, string):
+        self.contents = []
+        self.contents.append(string)
+        self.size = 14
+        self._set_bbox()
+        self.font = "sans-serif"
+
+    def draw(self, ctx):
         pass
 
+    def _set_bbox(self):
+        """resets the bounding box based on string contents
+        """
+        pass
 
 
 
